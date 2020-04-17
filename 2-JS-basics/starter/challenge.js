e433c35c43c687567f9ae189ca2eb70c98f342b1
MJoshua25 <jyao2000@yahoo.fr>
2020-04-17 22:38:03 +0000

    Coding challenge 1

---
+++ 2-JS-basics/starter/challenge.js
@@ -0,0 +1,16 @@
+/*************************************
+* Coding Challenge 1
+*/
+
+var heightJohn, heightMark, massJohn, massMark, bmiJohn, bmiMark, isMarkBmiHigher;
+
+heightJohn = parseFloat(prompt('Quelle est la taille de John ?'));
+massJohn = parseInt(prompt('Quelle est le poids de John ?'));
+heightMark = parseFloat(prompt('Quelle est la taille de Mark ?'));
+massMark = parseInt(prompt('Quelle est le poids de Mark ?'));
+
+bmiJohn = massJohn / (heightJohn * heightJohn);
+bmiMark = massJohn / (heightJohn * heightJohn);
+
+isMarkBmiHigher = bmiMark >= bmiJohn;
+console.log("Mark a t'il un plus haut BMI: " +isMarkBmiHigher)
