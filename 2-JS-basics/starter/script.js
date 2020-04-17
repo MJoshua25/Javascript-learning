397913734323e47199c6e5a0029d3f425769d8bb
MJoshua25 <jyao2000@yahoo.fr>
2020-04-17 22:25:28 +0000

    operators precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -86,5 +86,24 @@ var now =2018;
 var yearJohn = 1989;
 var fullAge = 18;
 
+// Multiple operators
 var isFullAge = now - yearJohn >= fullAge;
 console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = ( ageJohn + ageMark ) / 2;
+console.log(average);
+
+// Multiple assignements
+var x, y;
+x = y = ( 3 + 5 ) * 4 - 6;
+console.log(x, y);
+
+// More operators
+x *= 2; // x = x * 2
+console.log(x);
+x += 10;
+console.log(x);
+x++; // x = x+1
