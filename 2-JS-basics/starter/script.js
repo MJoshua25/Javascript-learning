36144d3b36c7d2e905b3d0e55df8fcec819b8498
MJoshua25 <jyao2000@yahoo.fr>
2020-04-17 05:34:18 +0000

    variable and data type

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -34,10 +34,13 @@ var job, isMarried;
 job = 'teacher';
 isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
 // Variable mutation
 age = 'twenty eight';
-job = 'driver'
+job = 'driver';
 
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
