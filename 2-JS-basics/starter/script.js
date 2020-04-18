b97640ce7df1c85df12f2f624bdaa07b1fad25b6
MJoshua25 <jyao2000@yahoo.fr>
2020-04-18 07:06:46 +0000

    Function

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -247,3 +247,11 @@ var ageJohn = calculateAge(1990);
 var ageMike = calculateAge(1948);
 var ageJane = calculateAge(1969);
 console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    console.log(firstName + ' retires in ' + retirement + ' years.');
+}
+
+yearsUntilRetirement(1990, 'John')
