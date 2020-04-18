655c39916d9ca17a7bdfb2cc20a1c863b52547f2
MJoshua25 <jyao2000@yahoo.fr>
2020-04-18 06:03:32 +0000

    Ternary Operator

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -172,7 +172,24 @@ if (age < 13) {
 var firstName = 'John';
 var age = 16;
 
+// Ternary Operator
 age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
 
 var drink = age >= 18 ? 'beer' : 'juice';
 console.log(drink);
+
+// Switch statement
+var job = 'teacher';
+switch (job) {
+    case "teacher":
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case "driver":
+        console.log(firstName + ' drives an uber in Lisbon.');
+        break;
+    case "designer":
+        console.log(firstName + ' design beutiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
