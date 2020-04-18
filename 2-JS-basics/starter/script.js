b3420d59b867939960a7644b7b30d62f5354a0ce
MJoshua25 <jyao2000@yahoo.fr>
2020-04-18 07:41:26 +0000

    Function Statement and Expressions

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -269,11 +269,22 @@ yearsUntilRetirement(1969, 'Jane');
 * Function Statement and Expressions
 */
 // Function declaration
-function whatDoYouDo(job, firstName) {
-
-}
+// function whatDoYouDo(job, firstName) {}
 
 // Function expression
 var whatDoYouDo = function (job, firstName) {
-
+    switch (job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code.';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.';
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default:
+            return firstName + ' does something else';
+    }
 }
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mike'));
