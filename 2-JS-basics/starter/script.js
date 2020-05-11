7226c80a25bf1463968e06bbcd0ceebd7f40d08f
Yao Josué <jyao2000@yahoo.fr>
2020-05-11 18:30:02 +0200

    Arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -295,3 +295,8 @@ console.log(whatDoYouDo('retired', 'Mike'));
 * Arrays
 */
 
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
