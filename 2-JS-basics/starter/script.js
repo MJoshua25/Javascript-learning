e89dac0d15e43265b09a2f7fea2cdb3d85c9bc72
Yao Josué <jyao2000@yahoo.fr>
2020-05-11 18:31:58 +0200

    Arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -295,12 +295,14 @@ console.log(whatDoYouDo('retired', 'Mike'));
 * Arrays
 */
 
+// Initialize new array
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1969, 1948);
 
 console.log(names[2]);
 console.log(names.length);
 
+// Mutate array data
 names[1] = 'Ben';
 names[names.length] = 'Mary';
 console.log(names);
